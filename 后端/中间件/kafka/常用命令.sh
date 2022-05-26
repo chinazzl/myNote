914d246d6ed991bfbd36853c428bdd17e557631c
zzl <coderzzl@163.com>
2022-05-26 09:06:55 +0800

    kafka properties

---
+++ 后端/中间件/kafka/常用命令.sh
@@ -0,0 +1,9 @@
+# 1. 先启动zookeeper
+./zookeeper-server-start.sh /config/zookeeper.properties
+
+# 2. 启动kafkaa
+./kafka-server-start.sh /config/server.properties
+
+# 3. 创建kafka主题
+# --zookeeper zookeeper 地址；--replication-factor 副本数量；--partitions 分区数量；--topic 创建主题
+./kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 3 --topic topic-first
